fd3e173e128208e1a60b9c2256529c9c126bf519
Tilzen <lucas.nascimentoR@hotmail.com>
2020-04-10 21:16:36 -0300

    schema - Adiciona mutation com registro de usuários.

--- lib/mini_blog_web/schema.ex
+++ lib/mini_blog_web/schema.ex
@@ -6,9 +6,17 @@ defmodule MiniBlogWeb.Schema do
   import_types MiniBlogWeb.Schema.Types
 
   query do
-    @desc "Get a list of all users"
+    @desc "Retorna uma lista com todos os usuários."
     field :users, list_of(:user_type) do
       resolve(&Resolvers.UserResolver.users/3)
+    end
+  end
+
+  mutation do
+    @desc "Registra um novo usuário."
+    field :register_user, type: :user_type do
+      arg :input, non_null(:user_input_type)
+      resolve(&Resolvers.UserResolver.register_user/3)
     end
   end
 end
