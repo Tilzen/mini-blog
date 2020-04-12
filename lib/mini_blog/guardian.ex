b7eb9ddf7dc9158950618ee0ceeedf1236cf24b6
Tilzen <lucas.nascimentoR@hotmail.com>
2020-04-12 17:28:43 -0300

    guardian - Corrige ordem das funções e corrige pattern transformando em map.

--- lib/mini_blog/guardian.ex
+++ lib/mini_blog/guardian.ex
@@ -6,11 +6,12 @@ defmodule MiniBlog.Guardian do
   def subject_for_token(%Accounts.User{} = user, _claims), do: {:ok, to_string(user.user_id)}
   def subject_for_token(_, _), do: {:error, :reason_for_error}
 
-  def resource_for_claims(_claims), do: {:error, :reason_for_error}
-  def resource_for_claims(%{sub: id}) do
+  def resource_from_claims(%{"sub" => id}) do
     case Accounts.get_user!(id) do
       nil -> {:error, :resource_not_found}
       user -> {:ok, user}
     end
   end
+  def resource_from_claims(_claims), do: {:error, :reason_for_error}
+
 end
