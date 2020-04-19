b051e100530526905e60acbb8683216fc42c0500
Tilzen <lucas.nascimentoR@hotmail.com>
2020-04-19 01:07:45 -0300

    schema - Adiciona mutation de comentários para o GraphQL.

--- lib/mini_blog_web/schema.ex
+++ lib/mini_blog_web/schema.ex
@@ -33,5 +33,12 @@ defmodule MiniBlogWeb.Schema do
       middleware(Middleware.Authorize, :any)
       resolve(&Resolvers.PostResolver.create_post/3)
     end
+
+    @desc "Cria um novo comentário"
+    field :create_comment, type: :comment_type do
+      arg :input, non_null(:comment_input_type)
+      middleware(Middleware.Authorize, :any)
+      resolve(&Resolvers.CommentResolver.create_comment/3)
+    end
   end
 end
